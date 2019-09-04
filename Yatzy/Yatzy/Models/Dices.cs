15d9ef7a239666c5f9c4114dde6a63bf35ff8231
Benjamin Ek <benjaminek@live.se>
2019-09-04 14:43:05 +0200

    Ändrade random i Dices

---
+++ Yatzy/Yatzy/Models/Dices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yatzy.Models
+{
+    class Dices
+    {
+        public void RollDices()
+        {
+            List<int> Dices = new List<int>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                Random random = new Random();
+                int rand = random.Next(2, 7);
+
+                Dices.Add(rand);
+            }
+        }
+    }
+}
