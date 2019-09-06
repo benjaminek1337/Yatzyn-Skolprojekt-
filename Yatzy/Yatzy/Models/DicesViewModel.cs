3fbf5087b65f515b1bdba25d2d738a7144e15238
Benjamin Ek <benjaminek@live.se>
2019-09-06 11:35:50 +0200

    Lagt till logik för tärningskast i klassen DicesViewModel. Experimentellt.

---
+++ Yatzy/Yatzy/Models/DicesViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yatzy.Models
+{
+    class DicesViewModel
+    {
+        public int DiceID { get; set; }
+        public int Dice { get; set; }
+        public bool IsDiceActive { get; set; }
+
+
+        public List<DicesViewModel> dices;
+
+        public DicesViewModel()
+        {
+            dices = new List<DicesViewModel>();
+            for (int i = 0; i < 5; i++)
+            {
+                dices[i].DiceID = i + 1;
+                dices[i].IsDiceActive = true;
+            }
+        }
+
+        public void SaveDice(List<DicesViewModel> dices, int diceButtonValue)
+        {
+            
+            for (int i = 0; i < dices.Count; i++)
+            {
+                if (DiceID == diceButtonValue)
+                {
+                    IsDiceActive = false;
+                }
+            }
+        }
+
+
+        public void RollDices(List<DicesViewModel> dices)
+        {
+            for (int i = 0; i < dices.Count; i++)
+            {
+                if (IsDiceActive)
+                {
+                    Random random = new Random();
+                    int rand = random.Next(1, 7);
+
+                    dices[i].Dice = rand;
+                }
+
+            }
+        }
+
+
+    }
+}
