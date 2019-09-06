72dd8f4dae828bc8759d3ef6fc973fd520130e75
MattiasS <47941846+FrogBoys@users.noreply.github.com>
2019-09-06 08:06:46 +0200

    La till frontend wow saker

--- Yatzy/Yatzy/MainWindow.xaml.cs
+++ Yatzy/Yatzy/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Media;
 
 namespace Yatzy
 {
@@ -24,7 +25,6 @@ namespace Yatzy
         {
             InitializeComponent();
         }
-
 
     }
 }
