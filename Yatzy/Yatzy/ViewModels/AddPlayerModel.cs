33d6ba9e0a2baf0f48e9f8f7461c01ae061d5c4a
Dan Johansson <dajo1800@student.miun.se>
2019-09-10 13:32:40 +0200

    Lagt till AddPlayerModel

---
+++ Yatzy/Yatzy/ViewModels/AddPlayerModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yatzy.Commands;
+using Yatzy.GameEngine;
+using Yatzy.Models;
+
+namespace Yatzy.ViewModels
+{
+    class AddPlayerModel : INotifyPropertyChanged
+    {
+        PlayerEngine playerengine;
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        #region Properties 
+        public RelayCommand SavePlayerCommand { get; set; }
+        public RelayCommand AddPlayerCommand { get; set; }
+
+        private ObservableCollection<Player> _players;
+        public ObservableCollection<Player> Players
+        {
+            get { return _players; }
+            set
+            {
+                _players = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Players"));
+            }
+        }
+
+        private Player player;
+        public Player Player
+        {
+            get { return player; }
+            set { player = value; PropertyChanged(this, new PropertyChangedEventArgs("Player")); }
+        }
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public void AddPlayer(string player)
+        {
+            if (Players.Count <= 4)
+            {
+                Player p = new Player
+                {
+                    Firstname ="",
+                    Lastname ="",
+                    Nickname =""
+
+                }; Players.Add(p);
+            }
+        }
+}
