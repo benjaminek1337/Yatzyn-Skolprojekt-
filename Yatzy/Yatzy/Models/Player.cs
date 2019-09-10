80d53918a2231a25ae8a95321bc7a7bd80f4ad09
Dan Johansson <dajo1800@student.miun.se>
2019-09-10 11:45:25 +0200

    Gjort om properties till "full properties" i Player.cs

--- Yatzy/Yatzy/Models/Player.cs
+++ Yatzy/Yatzy/Models/Player.cs
@@ -14,17 +14,74 @@ namespace Yatzy.Models
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        #region variabler till properties
+        private string _firstname;
+        private string _lastname;
+        private string _nickname;
 
         private int _ones;
-
-
-    
-        #region player properties
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Nickname { get; set; }
+        private int _twos;
+        private int _threes;
+        private int _fours;
+        private int _fives;
+        private int _sixes;
+        private int _upperScore;
+        private int _upperBonus;
+        private int _pair;
+        private int _twoPairs;
+        private int _threeOfaKind;
+        private int _fourOfaKind;
+        private int _smalLadder;
+        private int _largeLadder;
+        private int _fullHouse;
+        private int _chance;
+        private int _yatzy;
+        private int _totalScore;
         #endregion
-        #region dice results
+
+
+
+
+        #region player properties
+        public string Firstname
+        {
+            get { return _firstname; }
+            set
+            {
+                if (value != _firstname)
+                {
+                    _firstname = value;
+                    OnPropertyChanged("Firstname");
+                }
+            }
+        }
+        public string Lastname
+        {
+            get { return _lastname; }
+            set
+            {
+                if (value != _lastname)
+                {
+                    _lastname = value;
+                    OnPropertyChanged("Lastname");
+                }
+            }
+        }
+        public string Nickname
+        {
+            get { return _nickname; }
+            set
+            {
+                if (value != _lastname)
+                {
+                    _lastname = value;
+                    OnPropertyChanged("Lastname");
+                }
+            }
+        }
+        #endregion
+
+        #region dice result properties
         public int Ones
         {
             get { return _ones; }
@@ -36,30 +93,217 @@ namespace Yatzy.Models
                     OnPropertyChanged("Ones");
                 }
             }
-
         }
-        public int Twos { get; set; }
-        public int Threes { get; set; }
-        public int Fours { get; set; }
-        public int Fives { get; set; }
-        public int Sixes { get; set; }
-        public int UpperScore { get; set; }
-        public int UpperBonus { get; set; }
+        public int Twos
+        {
+            get { return _twos; }
+            set
+            {
+                if (value != _twos)
+                {
+                    _twos = value;
+                    OnPropertyChanged("Twos");
+                }
+            }
+        }
+        public int Threes
+        {
+            get { return _threes; }
+            set
+            {
+                if (value != _threes)
+                {
+                    _threes = value;
+                    OnPropertyChanged("Threes");
+                }
+            }
+        }
+        public int Fours
+        {
+            get { return _fours; }
+            set
+            {
+                if (value != _fours)
+                {
+                    _fours = value;
+                    OnPropertyChanged("Fours");
+                }
+            }
+        }
+        public int Fives
+        {
+            get { return _fives; }
+            set
+            {
+                if (value != _fives)
+                {
+                    _fives = value;
+                    OnPropertyChanged("Fives");
+                }
+            }
+        }
+        public int Sixes
+        {
+            get { return _sixes; }
+            set
+            {
+                if (value != _sixes)
+                {
+                    _sixes = value;
+                    OnPropertyChanged("Sixes");
+                }
+            }
+        }
+        public int UpperScore
+        {
+            get { return _upperScore; }
+            set
+            {
+                if (value != _upperScore)
+                {
+                    _upperScore = value;
+                    OnPropertyChanged("UpperScore");
+                }
+            }
+        }
+        public int UpperBonus
+        {
+            get { return _upperBonus; }
+            set
+            {
+                if (value != _upperBonus)
+                {
+                    _upperBonus = value;
+                    OnPropertyChanged("UpperBonus");
+                }
+            }
+        }
 
-        public int Pair { get; set; }
-        public int TwoPairs { get; set; }
-        public int ThreeOfaKind { get; set; }
-        public int FourOfaKind { get; set; }
-        public int SmalLadder { get; set; }
-        public int LargeLadder { get; set; }
-        public int FullHouse { get; set; }
-        public int Chance { get; set; }
-        public int Yatzy { get; set; }
+        public int Pair
+        {
+            get { return _pair; }
+            set
+            {
+                if (value != _pair)
+                {
+                    _pair = value;
+                    OnPropertyChanged("Pair");
+                }
+            }
+        }
+        public int TwoPairs
+        {
+            get { return _twoPairs; }
+            set
+            {
+                if (value != _twoPairs)
+                {
+                    _twoPairs = value;
+                    OnPropertyChanged("TwoPairs");
+                }
+            }
+        }
+        public int ThreeOfaKind
+        {
+            get { return _threeOfaKind; }
+            set
+            {
+                if (value != _threeOfaKind)
+                {
+                    _threeOfaKind = value;
+                    OnPropertyChanged("ThreeOfaKind");
+                }
+            }
+        }
+
+        public int FourOfaKind
+        {
+            get { return _fourOfaKind; }
+            set
+            {
+                if (value != _fourOfaKind)
+                {
+                    _fourOfaKind = value;
+                    OnPropertyChanged("FourOfaKind");
+                }
+            }
+        }
+        public int SmalLadder
+        {
+            get { return _smalLadder; }
+            set
+            {
+                if (value != _smalLadder)
+                {
+                    _smalLadder = value;
+                    OnPropertyChanged("SmalLadder");
+                }
+            }
+        }
+
+        public int LargeLadder
+        {
+            get { return _largeLadder; }
+            set
+            {
+                if (value != _largeLadder)
+                {
+                    _largeLadder = value;
+                    OnPropertyChanged("LargeLadder");
+                }
+            }
+        }
+
+        public int FullHouse
+        {
+            get { return _fullHouse; }
+            set
+            {
+                if (value != _fullHouse)
+                {
+                    _fullHouse = value;
+                    OnPropertyChanged("FullHouse");
+                }
+            }
+        }
+
+        public int Chance
+        {
+            get { return _chance; }
+            set
+            {
+                if (value != _chance)
+                {
+                    _chance = value;
+                    OnPropertyChanged("Chance");
+                }
+            }
+        }
+        public int Yatzy
+        {
+            get { return _yatzy; }
+            set
+            {
+                if (value != _yatzy)
+                {
+                    _yatzy = value;
+                    OnPropertyChanged("Yatzy");
+                }
+            }
+        }
+        
+        public int TotalScore
+        {
+            get { return _totalScore; }
+            set
+            {
+                if (value != _totalScore)
+                {
+                    _totalScore = value;
+                    OnPropertyChanged("TotalScore");
+                }
+            }
+        }
         #endregion
-        public int TotalScore { get; set; }
-
-
-
-
     }
 }
