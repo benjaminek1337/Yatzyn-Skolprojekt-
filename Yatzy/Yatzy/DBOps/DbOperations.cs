749d19aaae1352e2eae0c90373b76695c1a2fc5e
Johannes Lundkvist <johanneslundkvist@hotmail.com>
2019-09-11 14:03:35 +0200

    Databaskopplingar inlagda och DBOPS skapad

---
+++ Yatzy/Yatzy/DBOps/DbOperations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Npgsql;
+using System.Collections.ObjectModel;
+using Yatzy.Models;
+
+namespace Yatzy.DBOps
+{
+    class DbOperations
+    {
+
+        #region Metoder som hämtar data
+        public ObservableCollection<Player> GetPlayers()
+        {
+            Player p;
+
+            ObservableCollection<Player> players = new ObservableCollection<Player>();
+
+            using (var conn = new
+                NpgsqlConnection(ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT * FROM player";
+
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            p = new Player()
+                            {
+                                Nickname = reader.GetString(2),
+                                Firstname = reader.GetString(1),
+                                Lastname = reader.GetString(3)
+
+                            };
+
+                            players.Add(p);
+                        };
+                    }
+                }
+                return players;
+
+            }
+
+        }
+
+        #endregion
+    }
+}
