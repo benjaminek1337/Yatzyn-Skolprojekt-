c7fa73f83b2b7fafcfa61e576aef6384927bdb43
Johannes Lundkvist <johanneslundkvist@hotmail.com>
2019-09-12 10:24:07 +0200

    Metod för att testa transactions

--- Yatzy/Yatzy/DBOps/DbOperations.cs
+++ Yatzy/Yatzy/DBOps/DbOperations.cs
@@ -54,6 +54,90 @@ namespace Yatzy.DBOps
 
         }
 
+
+
+          public ObservableCollection<Player> GetPlayersTransaction()
+          {
+            Player p;
+
+            ObservableCollection<Player> players = new ObservableCollection<Player>();
+
+            using (var conn = new
+                NpgsqlConnection(ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString))
+            {
+
+                    NpgsqlTransaction transaction = null;
+                    string [] stmts = new string [1];
+                    stmts [0] = "SELECT * FROM player";
+                    stmts [1] = "SELECT * FROM players";
+
+                try 
+	            {	        
+		                conn.Open();
+
+                 using (var cmd = new NpgsqlCommand())
+                 {
+                    
+                    cmd.Connection = conn;
+                    transaction = conn.BeginTransaction();
+
+                    for (int i = 0; i < stmts.length; i++)
+			        {
+
+			        
+                       cmd.CommandText = stmts[i];
+
+                    
+
+                      using (var reader = cmd.ExecuteReader())
+                      {
+
+                        while (reader.Read())
+                        {
+                            p = new Player()
+                            {
+                                Nickname = reader.GetString(2),
+                                Firstname = reader.GetString(1),
+                                Lastname = reader.GetString(3)
+
+                            };
+
+                            players.Add(p);
+                        };
+                          
+
+                      }
+                    }
+                    transaction.Commit();
+                    return players;
+                    
+                 }
+	            }
+	            catch (Exception error)
+	            {
+	     	     transaction.Rollback();
+	            }
+
+                finally
+                {
+                    if (conn.State = System.Data.ConnectionState.Open)
+	                {
+                        conn.Close();
+	                }
+
+
+                }
+
+
+            }
+
+            
+
+            
+
+          }
+
+
         #endregion
     }
 }
