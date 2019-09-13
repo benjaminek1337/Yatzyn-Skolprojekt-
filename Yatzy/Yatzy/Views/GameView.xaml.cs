9680b81ba7fc2b1aa8decfa58bd858c996aa3c26
Dan Johansson <dajo1800@student.miun.se>
2019-09-13 11:24:13 +0200

    Utveckling av Grid och Datagrid för GameView

--- Yatzy/Yatzy/Views/GameView.xaml.cs
+++ Yatzy/Yatzy/Views/GameView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,51 @@ namespace Yatzy.Views
     /// <summary>
     /// Interaction logic for GameView.xaml
     /// </summary>
-    public partial class GameView : Page
+    public partial class GameView : Page, INotifyPropertyChanged
     {
+
+        private ObservableCollection<int> code;
+        public ObservableCollection<int> Code
+        {
+            get { return code; }
+            set
+            {
+                code = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Code"));
+            }            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+
         public GameView()
         {
             InitializeComponent();
-        }
+            //test.DataContext = this;
+            code = new ObservableCollection<int>();
+            code.Add(1);
+            code.Add(2);
+            code.Add(3);
+            code.Add(4);
+            code.Add(5);
+            code.Add(6);
+            code.Add(7);
+            code.Add(8);
+            code.Add(9);
+            code.Add(10);
+            code.Add(11);
+            code.Add(12);
+            code.Add(13);
+            code.Add(14);
+            code.Add(15);
+
+
+            this.DataContext = this;
+        }        
     }
 }
