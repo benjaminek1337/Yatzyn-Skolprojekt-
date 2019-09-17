7b0b04e84e8d4536fc6ddb473a93e21b8345721a
Benjamin Ek <benjaminek@live.se>
2019-09-17 12:07:16 +0200

    Piffat till CreateGameViewModel

--- Yatzy/Yatzy/ViewModels/CreateGameViewModel.cs
+++ Yatzy/Yatzy/ViewModels/CreateGameViewModel.cs
@@ -14,26 +14,68 @@ namespace Yatzy.ViewModels
 {
     class CreateGameViewModel : INotifyPropertyChanged
     {
-        DicesViewModel dicesViewModel;
-        PlayerEngine playerEngine; //Kasta in eller ut de valda spelarna till listan ActivePlayers eller vad fan den nu heter inne i PlayerEngine
-        DbOperations dbOps = new DbOperations();
-        
-        ObservableCollection<Player> players { get; set; }
-
 
         #region Properties
+        private ObservableCollection<Player> _players;
+        public ObservableCollection<Player> AvailablePlayers
+        {
+            get { return _players; }
+            set { _players = value; OnPropertyChanged("Players"); }
+        }
+
+        private Player _selectedPlayer;
+        public Player SelectedPlayer
+        {
+            get { return _selectedPlayer; }
+            set { _selectedPlayer = value; OnPropertyChanged("SelectedPlayer"); }
+        }
+        #endregion
+
+        #region Hårdkodad lista för testning
+        private ObservableCollection<Player> _hardcodedPlayers;
+
+        public ObservableCollection<Player> HardcodedPlayers
+        {
+            get { return _hardcodedPlayers; }
+            set { _hardcodedPlayers = value; OnPropertyChanged("HardcodedPlayers"); }
+        }
+
+        private void SetHardcodedPlayers()
+        {
+            Player p = new Player
+            {
+                Firstname = "Beendjaameeehn",
+                Lastname = "Ek",
+                Nickname = "Galne_Gunnar1337"
+            }; HardcodedPlayers.Add(p);
+            Player p2 = new Player
+            {
+                Firstname = "Djååohäänis",
+                Lastname = "Kuk",
+                Nickname = "ChildKillah"
+            }; HardcodedPlayers.Add(p2);
+            Player p3 = new Player
+            {
+                Firstname = "Määtiihuuuhs",
+                Lastname = "Svensson",
+                Nickname = "Skolrunken"
+            }; HardcodedPlayers.Add(p3);
+        }
+
+        #endregion
+
+        #region Objekt och lokala variabler
+
+        DicesViewModel dicesViewModel;
+        PlayerEngine playerEngine;
+        DbOperations dbOps = new DbOperations();
+
         RelayCommand ClassicGameCommand;
         RelayCommand StyrdGameCommand;
         RelayCommand AddPlayerCommand;
         RelayCommand RemovePlayerCommand;
         RelayCommand StartGameCommand;
-
-        public string Firstname { get; set; }
-        public string Nickname { get; set; }
-        public string Lastname { get; set; }
-
-        private int gameType = 4; //Ändra denna till 4 för klassisk eller 5 för styrd.
-        private Player selectedPlayer;
+        private int gameType = 0; //Ändra denna till 4 för klassisk eller 5 för styrd.
 
         #endregion
 
@@ -41,22 +83,19 @@ namespace Yatzy.ViewModels
 
         public CreateGameViewModel()
         {
-            /*ClassicGameCommand = new RelayCommand(, CanExecute);
-            StyrdGameCommand = new RelayCommand(, CanExecute);
-            AddPlayerCommand = new RelayCommand(, CanExecute);
-            RemovePlayerCommand = new RelayCommand(, CanExecute);
-            StartGameCommand = new RelayCommand(StartGame, CanExecute);*/
+            ClassicGameCommand = new RelayCommand(StyrdGame, CanChooseClassicYatzy);
+            StyrdGameCommand = new RelayCommand(ClassicGame, CanChooseStyrdYatzy);
+            AddPlayerCommand = new RelayCommand(AddPlayer, CanAddPlayer);
+            RemovePlayerCommand = new RelayCommand(RemovePlayer, CanRemovePlayer);
+            StartGameCommand = new RelayCommand(StartGame, CanStartGame);
 
-
-            GetAvaliablePlayers();
-
-           
-            //Ta bort kommentarsträcken när metoderna är skapade
+            //GetAvaliablePlayers();
+            SetHardcodedPlayers();
         }
 
         #endregion
 
-        #region intnu
+        #region PropChanged
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void OnPropertyChanged(string v)
@@ -67,22 +106,54 @@ namespace Yatzy.ViewModels
             }
         }
 
-        public bool CanExecute(object parameter)
-        {
-            return true;
-        }
         #endregion
 
-        public Player SelectedPlayer
+        #region bools
+        private bool CanStartGame(object parameter)
         {
-            get { return selectedPlayer; }
-            set { selectedPlayer = value; OnPropertyChanged("SelectedPlayer"); }
+            if (playerEngine.ActivePlayers.Count > 1 && playerEngine.ActivePlayers.Count < 5 && gameType == 4 || gameType == 5)
+                return true;
+            else
+                return false;
         }
 
+        private bool CanAddPlayer(object parameter)
+        {
+            if (playerEngine.ActivePlayers.Count < 5)
+                return true;
+            else
+                return false;                    
+        }
+        private bool CanRemovePlayer(object parameter)
+        {
+            if (playerEngine.ActivePlayers.Count > 0)
+                return true;
+            else
+                return false;
+        }
 
+        private bool CanChooseClassicYatzy(object parameter)
+        {
+            if (CanChooseStyrdYatzy(parameter))
+                return false;
+            else
+                return true;
+        }
+
+        private bool CanChooseStyrdYatzy(object parameter)
+        {
+            if (CanChooseClassicYatzy(parameter))
+                return false;
+            else
+                return true;                
+        }
+
+        #endregion
+
+        #region Metoder
         private void GetAvaliablePlayers()
         {
-            players = dbOps.GetAvaliablePlayers();
+            AvailablePlayers = dbOps.GetAvaliablePlayers();
         }
 
         public void RemovePlayer(object parameter)
@@ -90,19 +161,26 @@ namespace Yatzy.ViewModels
             playerEngine.ActivePlayers.Remove(SelectedPlayer);
         }
 
-
-        public void AddPlayerToGame(object parameter)
+        public void AddPlayer(object parameter)
         {
             playerEngine.ActivePlayers.Add(SelectedPlayer);
         }
 
+        private void ClassicGame(object parameter)
+        {
+            gameType = int.Parse(parameter.ToString());
+        }
 
+        private void StyrdGame(object parameter)
+        {
+            gameType = int.Parse(parameter.ToString());
+        }
 
         private void StartGame(object parameter)
         {
             dicesViewModel = new DicesViewModel(gameType);
         }
 
-
+        #endregion
     }
 }
